cb791d5ec02da9e9558c09a213c6e01f5f1102a2
Sophian-Asaidi <132987804+Sophian-Asaidi@users.noreply.github.com>
2023-10-17 11:41:24 +0200

    maj gestion_bien

--- sitelocation/traitement/gestion_bien.php
+++ sitelocation/traitement/gestion_bien.php
@@ -21,22 +21,46 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_POST['addBien'])) {
 
 if ($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_POST['updateBien'])) {
     $id = $_POST['id'];
-    $data = [
-        ':nom' => $_POST['nom'],
-        ':rue' => $_POST['rue'],
-        ':cop' => $_POST['cop'],
-        ':vil' => $_POST['vil'],
-        ':sup' => $_POST['sup'],
-        ':nbCouchage' => $_POST['nb_couchage'],
-        ':nbPiece' => $_POST['nb_piece'],
-        ':nbChambre' => $_POST['nb_chambre'],
-        ':descriptif' => $_POST['descriptif'],
-        ':ref_bien' => $_POST['ref_bien'],
-        ':statu' => $_POST['statu_bien'],
-        ':idType' => $_POST['id_type_bien']
-    ];
+    $data = [':id' => $id];    
+    if (!empty($_POST['nom'])) {
+        $data[':nom'] = $_POST['nom'];
+    }
+    if (!empty($_POST['rue'])) {
+        $data[':rue'] = $_POST['rue'];
+    }
+    if (!empty($_POST['cop'])) {
+        $data[':cop'] = $_POST['cop'];
+    }
+    if (!empty($_POST['vil'])) {
+        $data[':vil'] = $_POST['vil'];
+    }
+    if (!empty($_POST['sup'])) {
+        $data[':sup'] = $_POST['sup'];
+    }
+    if (!empty($_POST['nb_couchage'])) {
+        $data[':nbCouchage'] = $_POST['nb_couchage'];
+    }
+    if (!empty($_POST['nb_piece'])) {
+        $data[':nbPiece'] = $_POST['nb_piece'];
+    }
+    if (!empty($_POST['nb_chambre'])) {
+        $data[':nbChambre'] = $_POST['nb_chambre'];
+    }
+    if (!empty($_POST['descriptif'])) {
+        $data[':descriptif'] = $_POST['descriptif'];
+    }
+    if (!empty($_POST['ref_bien'])) {
+        $data[':ref_bien'] = $_POST['ref_bien'];
+    }
+    if (!empty($_POST['statu_bien'])) {
+        $data[':statu'] = $_POST['statu_bien'];
+    }
+    if (!empty($_POST['id_type_bien'])) {
+        $data[':idType'] = $_POST['id_type_bien'];
+    }
     Bien::updateBien($id, $data);
 }
+
 
 if ($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_POST['deleteBien'])) {
     $id = $_POST['delete_id'];
@@ -115,18 +139,18 @@ $biens = Bien::getAllBiens();
         <h2>Modifier un Bien</h2>
         <form method="post">
             <input type="text" name="id" placeholder="ID du Bien à mettre à jour" required>
-            <input type="text" name="nom" placeholder="Nouveau Nom">
-            <input type="text" name="rue" placeholder="Nouvelle Adresse">
-            <input type="text" name="cop" placeholder="Nouveau Code Postal">
-            <input type="text" name="vil" placeholder="Nouvelle Ville">
+            <input type="text" name="nom" placeholder="Nouveau Nom" required>
+            <input type="text" name="rue" placeholder="Nouvelle Adresse" required>
+            <input type="text" name="cop" placeholder="Nouveau Code Postal" required>
+            <input type="text" name="vil" placeholder="Nouvelle Ville" required>
             <input type="text" name="sup" placeholder="Nouvelle Surface">
-            <input type="text" name="nb_couchage" placeholder="Nouveau Nombre de Couchages">
-            <input type="text" name="nb_piece" placeholder="Nouveau Nombre de Pièces">
-            <input type="text" name="nb_chambre" placeholder="Nouveau Nombre de Chambres">
-            <input type="text" name="descriptif" placeholder="Nouveau Descriptif">
-            <input type="text" name="ref_bien" placeholder="Nouvelle Référence">
-            <input type="text" name="statu_bien" placeholder="Nouveau Statut">
-            <input type="text" name="id_type_bien" placeholder="Nouvel ID du Type de Bien">
+            <input type="text" name="nb_couchage" placeholder="Nouveau Nombre de Couchages" required>
+            <input type="text" name="nb_piece" placeholder="Nouveau Nombre de Pièces" required>
+            <input type="text" name="nb_chambre" placeholder="Nouveau Nombre de Chambres"required>
+            <input type="text" name="descriptif" placeholder="Nouveau Descriptif"required>
+            <input type="text" name="ref_bien" placeholder="Nouvelle Référence" required>
+            <input type="text" name="statu_bien" placeholder="Nouveau Statut" required>
+            <input type="text" name="id_type_bien" placeholder="Nouvel ID du Type de Bien" required>
             <button type="submit" name="updateBien">Mettre à jour</button>
         </form>
     </body>
